d7d179b3ff29c187f9bc65201518e4a31291c3e7
thebaht <31901333+thebaht@users.noreply.github.com>
2024-10-25 12:23:22 +0200

    *

--- 02/04/northwind.py
+++ 02/04/northwind.py
@@ -11,34 +11,48 @@ db = connector.connect(
     use_pure=True
 )
 
-cursor = db.cursor()
+
 
 orders = pd.read_sql("select * from orders",db)
 employees = pd.read_sql("select * from employees",db)
-
+shippers = pd.read_sql("select * from shippers",db)
 db.close()
 
 
-bEC = pd.merge(orders, employees, on="EmployeeID", how="inner").groupby("Country").size().reset_index(name="count")
+oBcountry = orders.groupby("ShipCountry")["OrderID"].count().reset_index(name="count")
+
+oBshipper = pd.merge(orders, shippers, left_on="ShipVia", right_on="ShipperID").groupby("CompanyName").size().reset_index(name="count")
+
+orders['OrderDate'] = pd.to_datetime(orders["OrderDate"])
+orders['year_month'] = orders["OrderDate"].dt.to_period(('M'))
+oBtime = orders.groupby(['year_month']).size().reset_index(name="count")
+oBtime['year_month'] = oBtime['year_month'].dt.to_timestamp()
 
 
-bSC = orders.groupby("ShipCountry")["OrderID"].count().reset_index(name="count")
 
-
-plt.figure(figsize=(18, 5))
-
-plt.subplot(1, 1, 1)
-plt.bar(bSC["ShipCountry"], bSC["count"], color='skyblue')
+plt.figure(figsize=(15, 5))
+plt.bar(oBcountry["ShipCountry"], oBcountry["count"], color='skyblue')
 plt.title('Orders by Shipping country')
 plt.ylabel('Count')
+plt.xticks(rotation=45) 
 plt.tight_layout()
 
-plt.figure(figsize=(4, 5))
-plt.subplot(1, 1, 1)
-plt.bar(bEC["Country"], bEC["count"], color='skyblue')
-plt.title('Orders by Employee country')
+
+
+plt.figure(figsize=(6, 5))
+plt.bar(oBshipper["CompanyName"], oBshipper["count"], color='skyblue')
+plt.title('Orders by Shipper')
 plt.ylabel('Count')
 plt.tight_layout()
 
+
+plt.figure(figsize=(7, 7))
+plt.plot(oBtime["year_month"], oBtime["count"], marker='o', linestyle='-', color='b')  # 'o' for circle markers
+plt.title('Monthly Orders Over Time')
+plt.xlabel('Year-Month')
+plt.ylabel('Orders')
+plt.xticks(rotation=45) 
+plt.grid(True)
+plt.tight_layout()
 
 plt.show()
