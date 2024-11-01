57b4a972b5ac2fff1ab784b5eadcb04bf6f34081
thebaht <31901333+thebaht@users.noreply.github.com>
2024-11-01 15:39:22 +0100

    asd

--- 04/04.py
+++ 04/04.py
@@ -3,6 +3,8 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 import operator
 import cProfile
+import multiprocessing
+import time
 
 #create profiler
 profiler = cProfile.Profile()
@@ -77,90 +79,117 @@ def select(states, line=(None, None), depth=('==', None), centipawn=('==', None)
 csv.reader -> list<list>
 selects rows where mate == 20.0. 
 Counts the amount of rows, calculates their average/max/min depth, prints the result to console and returns lists of values for charts'''
-def eval1(reader):
-    source.seek(0)  # start from the beginning of the file
-    next(reader)    # skip the header
-    selected = select( reader, mate=('==', 20.0))   # select the rows 
-    a, c, max, min, freq = 0, 0, 0, 0, defaultdict(int) # create variables for stats
-    for l in selected:              # go through the selected rows for analysis
-        freq[ l['depth'] ] += 1     # count frequency of depth
-        a += l['depth']             # sum total depth
-        c += 1                      # count the number of rows
-        if max < l['depth']:        # check for maximum
-            max = l['depth']        # store maximum
-        if min > l['depth']:        # check for minimum
-            min = l['depth']        # store minimum
-    
-    # print data to console
-    print(f"\nEval1:\nDepth for {c} mate evaluations of 20.0: \n\tAverage:\t{a/c}\n\tMax:\t{max}\n\tMin:\t{min}")
-    freq = sorted(freq.items(), key=lambda x: x)    # sort frequncies for nicer charts
-    lists = [[item[0] for item in freq], [item[1] for item in freq]]    # organise depths and frequencies in list<list> for charts
-    return lists    # return list<list> for charts
+def eval1(queue, file_path):
+    try:
+        with open(file_path, 'r') as source: # open file
+            reader = csv.reader(source) # create reader
+            next(reader)    # skip the header
+            selected = select( reader, mate=('==', 20.0))   # select the rows 
+            a, c, max, min, freq = 0, 0, 0, 0, defaultdict(int) # create variables for stats
+            for l in selected:              # go through the selected rows for analysis
+                freq[ l['depth'] ] += 1     # count frequency of depth
+                a += l['depth']             # sum total depth
+                c += 1                      # count the number of rows
+                if max < l['depth']:        # check for maximum
+                    max = l['depth']        # store maximum
+                if min > l['depth']:        # check for minimum
+                    min = l['depth']        # store minimum
+            
+            # print data to console
+            print(f"\nEval1:\nDepth for {c} mate evaluations of 20.0: \n\tAverage:\t{a/c}\n\tMax:\t{max}\n\tMin:\t{min}")
+            freq = sorted(freq.items(), key=lambda x: x)    # sort frequncies for nicer charts
+            lists = [[item[0] for item in freq], [item[1] for item in freq]]    # organise depths and frequencies in list<list> for charts
+            queue.put( lists)  # # put results in queue
+            #Error messages if something breaks while running
+    except FileNotFoundError:   
+        print(f"Error: The file '{file_path}' was not found.")
+    except PermissionError:
+        print(f"Error: You do not have permission to access '{file_path}'.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 '''
 csv.reader -> list<list>
 selects rows where centipawn is empty. 
 Counts the amount of rows, calculates their average/max/min depth, prints the result to console and returns lists of values for charts'''
-def eval2(reader):
-    source.seek(0)      # start from the beginning of the file
-    next(reader)        # skip the header
-    selected = select( reader, centipawn=('==', ''))  # select the rows 
-    a, c, max, min, freq = 0, 0, 0, 0, defaultdict(int) # create variables for stats
-    for l in selected:              # go through the selected rows for analysis
-        freq[ l['depth'] ] += 1     # count frequency of depth    
-        a += l['depth']             # sum total depth
-        c += 1                      # count the number of rows
-        if max < l['depth']:        # check for maximum
-            max = l['depth']        # store maximum
-        if min > l['depth']:        # check for minimum
-            min = l['depth']        # store minimum
-            
-    # print data to console
-    print(f"\nEval2:\nDepth for {c} evaluations where mate is certain: \n\tAverage:\t{a/c}\n\tMax:\t{max}\n\tMin:\t{min}")
-    freq = sorted(freq.items(), key=lambda x: x)    # sort frequncies for nicer charts
-    lists = [[item[0] for item in freq], [item[1] for item in freq]]   # organise depths and frequencies in list<list> for charts
-    return lists    # return list<list> for charts
-
+def eval2(queue, file_path):
+    try:
+        with open(file_path, 'r') as source: # open file
+            reader = csv.reader(source) # create reader
+            next(reader)        # skip the header
+            selected = select( reader, centipawn=('==', ''))  # select the rows 
+            a, c, max, min, freq = 0, 0, 0, 0, defaultdict(int) # create variables for stats
+            for l in selected:              # go through the selected rows for analysis
+                freq[ l['depth'] ] += 1     # count frequency of depth    
+                a += l['depth']             # sum total depth
+                c += 1                      # count the number of rows
+                if max < l['depth']:        # check for maximum
+                    max = l['depth']        # store maximum
+                if min > l['depth']:        # check for minimum
+                    min = l['depth']        # store minimum
+                
+            # print data to console
+            print(f"\nEval2:\nDepth for {c} evaluations where mate is certain: \n\tAverage:\t{a/c}\n\tMax:\t{max}\n\tMin:\t{min}")
+            freq = sorted(freq.items(), key=lambda x: x)    # sort frequncies for nicer charts
+            lists = [[item[0] for item in freq], [item[1] for item in freq]]   # organise depths and frequencies in list<list> for charts
+            queue.put(lists)    # put results in queue
+    #Error messages if something breaks while running
+    except FileNotFoundError:   
+        print(f"Error: The file '{file_path}' was not found.")
+    except PermissionError:
+        print(f"Error: You do not have permission to access '{file_path}'.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 # start the profiler     
 profiler.enable()
 
-file_path = 'less_evals.csv' # file to read from       
-# file_path = 'evals.csv'   # file to read from
-try:     
-    with open(file_path, 'r') as source: # open file
-        reader = csv.reader(source) # create reader
-        e1 = eval1(reader)  # run the 1st evaluation 
-        e2 = eval2(reader)  # run the 2nd evaluation
+# File to read from
+file_path = 'less_evals.csv'       
+# file_path = 'evals.csv'  
 
-        print(f"\n_______________\ncProfile:\n")  # to make console more readable
-        profiler.print_stats()  # print profiler stats
-#Error messages if something breaks while running
-except FileNotFoundError:   
-    print(f"Error: The file '{file_path}' was not found.")
-except PermissionError:
-    print(f"Error: You do not have permission to access '{file_path}'.")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+if __name__ == '__main__':
+    # create queues for storing results
+    queue1 = multiprocessing.Queue()    
+    queue2 = multiprocessing.Queue()  
     
+    # create processes for reading and processing data
+    process1 = multiprocessing.Process(target=eval1, args=(queue1, file_path)) 
+    process2 = multiprocessing.Process(target=eval2, args=(queue2, file_path)) 
+    
+    # start processes
+    process1.start()
+    process2.start() 
+
+    # wait for both processes to finish and then terminate them
+    process1.join()
+    process2.join()
+    
+    # extract results
+    e1 = queue1.get()
+    e2 = queue2.get()
+    
+    print(f"\n_______________\ncProfile:\n")  # to make console more readable
+    profiler.print_stats()  # print profiler stats
     
 
 
-# create bar chart for the 1st evaluation
-plt.figure(figsize=(15, 5))
-plt.bar(e1[0], e1[1], color='skyblue')   
-plt.title('Depth Frequency - Mate evaluation of 20.0')                  
-plt.xlabel('Depth')                           
-plt.ylabel('Frequency')                           
-plt.tight_layout()
 
-# create bar chart for the 2nd evaluation
-plt.figure(figsize=(15, 5))
-plt.bar(e2[0], e2[1], color='skyblue')   
-plt.title('Depth Frequency - When mate is certain')                  
-plt.xlabel('Depth')                           
-plt.ylabel('Frequency')                           
-plt.tight_layout()
+    # create bar chart for the 1st evaluation
+    plt.figure(figsize=(15, 5))
+    plt.bar(e1[0], e1[1], color='skyblue')   
+    plt.title('Depth Frequency - Mate evaluation of 20.0')                  
+    plt.xlabel('Depth')                           
+    plt.ylabel('Frequency')                           
+    plt.tight_layout()
 
-# Show both charts
-plt.show()
+    # create bar chart for the 2nd evaluation
+    plt.figure(figsize=(15, 5))
+    plt.bar(e2[0], e2[1], color='skyblue')   
+    plt.title('Depth Frequency - When mate is certain')                  
+    plt.xlabel('Depth')                           
+    plt.ylabel('Frequency')                           
+    plt.tight_layout()
+
+    # Show both charts
+    plt.show()
