7cd5b24da0139b7febae2448969301db002d29f9
Frederik Baht-Hagen <fbah@itu.dk>
2024-11-07 09:54:38 +0100

    awsd

--- 05/pdf_downloader.py
+++ 05/pdf_downloader.py
@@ -8,6 +8,7 @@ from PIL import Image, ImageTk
 import threading
 
 
+
 source_path = 'GRI_2017_2020.xlsx'
 output_path = './pdf-files/'
 makedirs('pdf-files', exist_ok=True)
@@ -16,22 +17,22 @@ labels = {}
 downloaded = set()
 failed = set()
 
+
+print(f'\nReading file..')
 df = pd.read_excel(source_path, index_col='BRnum')
 df = df[df.Pdf_URL.notnull() == True]
 df = df[['Pdf_URL', 'Report Html Address']] # removes irrelevant columns. Not necessary to do, but it only adds 0.01s and feels cleaner.
 
+for (dirpath, dirnames, filenames) in walk(output_path):
+    downloaded.update(map(lambda x: x[:-4], filenames))
 
 arrow_path = './icons/square-arrow-down.png'
 check_path = './icons/square-check.png'
 x_path = './icons/square-x.png'
 
-
 root = tk.Tk()                          # Create main window
-root.geometry("200x600")                # Set window dimensions
+root.geometry("760x600")                # Set window dimensions
 root.config(bg='gray6')              # Set window color
-
-# frame = tk.Frame(root)
-# frame.pack()
 
 arrow_icon = ImageTk.PhotoImage(Image.open(arrow_path))
 check_icon = ImageTk.PhotoImage(Image.open(check_path))
@@ -39,8 +40,6 @@ x_icon = ImageTk.PhotoImage(Image.open(x_path))
 
 global_font = font.Font(family='Verdana', size=12)
 
-for (dirpath, dirnames, filenames) in walk(output_path):
-    downloaded.update(map(lambda x: x[:-4], filenames))
 
 # Create a style for the scrollbar
 style = ttk.Style(root)
@@ -67,7 +66,7 @@ style.layout("Custom.Vertical.TScrollbar",
 
 canvas = tk.Canvas(root, bg='gray6', bd=3, highlightbackground='gray6', highlightthickness=3)
 scrollbar = ttk.Scrollbar(root, orient="vertical", command=canvas.yview, style="Custom.Vertical.TScrollbar")
-scrollable_frame = tk.Frame(canvas, bg='gray6')
+frame = tk.Frame(canvas, padx=24, pady=14, bg='gray6')
 
 # Function to scroll on Windows (mouse wheel event binding)
 def on_mouse_wheel(event):
@@ -76,12 +75,12 @@ def on_mouse_wheel(event):
 # Bind mouse wheel events to scroll on different OS
 canvas.bind_all("<MouseWheel>", on_mouse_wheel)       # Windows
 
-scrollable_frame.bind(
+frame.bind(
     "<Configure>",
     lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
 )
 
-canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+canvas.create_window((0, 0), window=frame, anchor="nw")
 canvas.configure(yscrollcommand=scrollbar.set)
 
 scrollbar.pack(side="right", fill="y")
@@ -108,23 +107,32 @@ def download(path, brnum, url, alt_url=pd.NA):
 
 def main():
     with ThreadPoolExecutor() as executor:
-        for i, row in df.head(20).iterrows():
+        for brnum, row in df.head(20).iterrows():
         # for i, row in df.iterrows():
-            if i in downloaded:
-                labels[i].config(image=check_icon)
+            if brnum in downloaded:
+                labels[brnum].config(image=check_icon)
                 continue
-            path = output_path+i+'.pdf'
-            executor.submit(download, path, i, row['Pdf_URL'], row['Report Html Address'])
+            path = output_path+brnum+'.pdf'
+            executor.submit(download, path, brnum, row['Pdf_URL'], row['Report Html Address'])
+            
+    with open(output_path+'_status.txt', 'w') as output:
+        for brnum, row in df.head(20).iterrows():
+            if brnum in failed:
+                status = 'Failed'
+            else:
+                status = 'Downloaded'
+            output.write(f"{brnum}\t\t{status}\n")
 
 
-
-for i, row in df.head(20).iterrows():
+for i, (brnum, row) in enumerate(df.head(20).iterrows()):
 # for i, row in df.iterrows():
-    label = tk.Label(scrollable_frame, text=f"{i}", padx=5, image=arrow_icon, compound='right', font=global_font, fg='gray90', bg='gray6')
-    label.pack(padx=44, pady=1)
-    labels[i]=label
+    row = i // 5
+    col = i % 5
+    label = tk.Label(frame, text=f"{brnum}", padx=5, image=arrow_icon, compound='right', font=global_font, fg='gray90', bg='gray6')
+    label.grid(row=row, column=col, padx=6, pady=2)
+    # label.pack(padx=44, pady=1)
+    labels[brnum]=label
 
 thread = threading.Thread(target=main)
 thread.start()
-# root.after(100, main)
 root.mainloop()
